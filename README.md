6d6359398f063cc7d5dbab14831aa9e2b8a9b861
Genjun Liu <genjun.liu@liulishuo.com>
2019-03-24 14:43:53 +0800

    update README

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # bazel_cross_compile
+
+## linux
+    bazel test //... --config=linux
+
+## darwin & ios
+  - take a look at .bazelrc
+  - set platform type of ios (--ios-multi-cpus)
+  - build
+    ```
+    bazel test //... --config=darwin
+    ```
+  
+    Note: If you want to build a library for iOS, you need to use `apple_static_library` in the BUILD file.
+    
+## android
+  - download Android NDK
+  - set `ANDROID_NDK_HOME` environment variable
+  - build
+    ```
+    bazel test //... --config=android_armeabi-v7a
+    bazel test //... --config=android_arm64-v8a
+    bazel test //... --config=android_simulator_x86
+    ```
+    
+## TODO
+    Disable Android test builds with configuration settings
--- src/sample.cc
+++ src/sample.cc
@@ -1,30 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include "math.h"
-//#include "gflags/gflags.h"
-//#include "glog/logging.h"
 
 void PrintFile(const char *filename) {
     std::string line;
     std::ifstream ifs(filename);
-    //LOG(INFO) << "Output file:" << filename;
     while (std::getline(ifs, line)) {
         std::cout << line << std::endl;
     }
     ifs.close();
-    //LOG(INFO) << "Output end.";
 }
-
-//DEFINE_int32(operand_1st, 0, "the first operand.");
-//DEFINE_int32(operand_2nd, 0, "the first operand.");
 
 int main(int argc, char*argv[]) {
     std::cout << "hello world." << std::endl;
     std::cout << math_lib::max(1,2) << std::endl;
-    //google::InitGoogleLogging(argv[0]);
-    //gflags::ParseCommandLineFlags(&argc, &argv, true);
-    //PrintFile("./testdata/mytestdata/hello.txt");
-    //PrintFile("./testdata/mytestdata/echo.txt");
-    //LOG(INFO) << "max(operand_1st, operand_2nd) is " << max(FLAGS_operand_1st, FLAGS_operand_2nd);
     return 0;
 }
